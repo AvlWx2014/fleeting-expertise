7b881147e5474af423125de4401c8e885a5089d5
Ryan Smith <avlwx2014@gmail.com>
2023-04-06 08:22:41 -0400

    Add convenient KojiService extension methods for the data layer

---
+++ src/jvmMain/kotlin/data/net/KojiServiceExtensions.kt
@@ -0,0 +1,45 @@
+package data.net
+
+import data.net.xmlrpc.MethodResponse
+import data.net.xmlrpc.methodCall
+import retrofit2.Call
+
+fun KojiService.getBuild(nvr: String, strict: Boolean = false): Call<MethodResponse> = post(
+    methodCall("getBuild", nvr, strict)
+)
+
+fun KojiService.getRpmsForBuild(buildId: Int): Call<MethodResponse> = post(
+    methodCall("listRPMs", buildId)
+)
+
+fun KojiService.getBuildrootListing(buildrootId: Int): Call<MethodResponse> = post(
+    methodCall(
+        "getBuildrootListing",
+        buildrootId
+    )
+)
+
+fun KojiService.getRpm(id: Int, strict: Boolean = false, multi: Boolean = false): Call<MethodResponse> =
+    post(methodCall("getRpm", id, strict, multi))
+
+fun KojiService.getRpm(nvra: String, strict: Boolean = false, multi: Boolean = false): Call<MethodResponse> =
+    post(methodCall("getRpm", nvra, strict, multi))
+
+fun KojiService.getRpm(
+    name: String,
+    version: String,
+    release: String,
+    arch: String,
+    strict: Boolean = false,
+    multi: Boolean = false
+): Call<MethodResponse> = getRpm("$name-$version-${release}.$arch", strict, multi)
+
+fun KojiService.getRpm(
+    name: String,
+    version: String,
+    release: String,
+    arch: String,
+    location: String,
+    strict: Boolean = false,
+    multi: Boolean = false
+): Call<MethodResponse> = getRpm("$name-$version-${release}.$arch@$location", strict, multi)
