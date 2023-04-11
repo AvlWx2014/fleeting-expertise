72dfb6c458047e5d9c663caa14554e811108ca19
Ryan Smith <avlwx2014@gmail.com>
2023-04-11 13:26:32 -0400

    Hack in initial main entrypoint

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,33 +1,29 @@
 @file:JvmName("Main")
 
-import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import data.net.*
+import data.net.xmlrpc.*
+import view.MainScreen
+import viewmodel.MainScreenViewModel
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
-        }
-    }
+    val applicationScope = rememberCoroutineScope()
+    val viewModel = MainScreenViewModel(parentScope = applicationScope)
+    MainScreen(viewModel)
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(
+        title = "The Tree Of Time",
+        onCloseRequest = ::exitApplication
+    ) {
         App()
     }
 }
